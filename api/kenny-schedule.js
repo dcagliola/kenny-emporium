b508921c728d8701c92b0e0a9d19d014f67b2687
Cam78212 <cjg6488@psu.edu>
2025-12-14 18:17:55 -0500

    fixed naming

--- api/kenny-schedule.js
+++ api/kenny-schedule.js
@@ -5,11 +5,11 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 export default function handler(req, res) {
-  const filePath = join(process.cwd(), 'api/kenny-schedule.json');
+  const filePath = join(process.cwd(), 'api/kenny-program.json');
   const data = JSON.parse(readFileSync(filePath, 'utf8'));
   
   // Extract the schedule array from the object
-  const schedule = data['kenny-schedule'] || [];
+  const schedule = data['kenny-program'] || [];
   
   res.status(200).json(schedule);
 }
